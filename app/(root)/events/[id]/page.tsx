93b00f8f59e95d5b76f7e0369e6456e57b13d390
raefka <raefkacem20@gmail.com>
2024-01-25 13:27:03 +0100

    implement stripe2

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,4 +1,4 @@
-import CheckoutButton from '@/components/shared/CheckOutButton';
+import CheckoutButton from '@/components/shared/CheckoutButton';
 import Collection from '@/components/shared/Collection';
 import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions'
 import { formatDateTime } from '@/lib/utils';
