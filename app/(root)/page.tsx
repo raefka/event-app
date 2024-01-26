6293d328aae99f57bca73ed2195a55aefa82e200
raefka <raefkacem20@gmail.com>
2024-01-26 17:00:09 +0100

    third commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
+import CategoryFilter from '@/components/shared/CategoryFilter';
 import Collection from '@/components/shared/Collection';
+import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button';
 import { getAllEvents } from '@/lib/actions/event.actions';
+import { SearchParamProps } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default async function Home() {
-  const events = await getAllEvents({query :'',category:'',page:1,limit:6});
+export default async function Home({ searchParams }: SearchParamProps) {
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || '';
+  const category = (searchParams?.category as string) || '';
+
+  const events = await getAllEvents({query :searchText,category,page,limit:6});
   console.log(events)
   return (
     <>
@@ -34,8 +41,8 @@ export default async function Home() {
           <h2 className='h2-bold'>Trust by <br/> Thousands of Events</h2>
 
           <div className='flex flex-full flex-col gap-5 md:flex-row'>
-            search
-            CategoryFilter
+            <Search />
+            <CategoryFilter />
           </div>
 
           <Collection
--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,6 +1,8 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
@@ -9,6 +11,10 @@ const ProfilePage = async () => {
    const { sessionClaims } = auth();
 
    const userId = sessionClaims?.userId as string;
+
+   const orders = await getOrdersByUser({userId,page:1});
+
+   const orderedEvents = orders?.data.map((order : IOrder) => order.event) || [];
 
    const organizedEvents = await getEventsByUser({userId , page:1});
    
@@ -25,9 +31,9 @@ const ProfilePage = async () => {
           </div>
       </section>
 
-       {/* <section className='wrapper my-8'>
+        <section className='wrapper my-8'>
          <Collection
-            data={events?.data}
+            data={orderedEvents}
             emptyTitle="No Event tickets purchased yet"
             emptyStateSubtext="No worries - plenty of exciting events to explore !"
             collectionType="My_Tickets"
@@ -36,7 +42,7 @@ const ProfilePage = async () => {
             urlParamName='ordersPage'
             totalPages={2}
           />
-        </section>*/}
+        </section>
 
        <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
           <div className='wrapper flex items-center justify-center sm:justify-between'>
